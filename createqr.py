1baeb9717d67ac9f0c1e43e42bd0e9510e1d65d2
Preben Madsen <839803+prebenlm@users.noreply.github.com>
2023-04-08 12:11:15 +0200

    updates

--- createqr.py
+++ createqr.py
@@ -1,12 +1,13 @@
 import qrcode
 from PIL import Image, ImageDraw, ImageFont
 import os
+import re
 
 def create_qr_code(text, output_path):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
+        box_size=12,
         border=1,
     )
     qr.add_data(text)
@@ -15,23 +16,23 @@ def create_qr_code(text, output_path):
     img = qr.make_image(fill_color="black", back_color="transparent")
     return img
 
-def get_arial_font_path():
+def get_font_path():
     if os.sys.platform.startswith('win'):
-        font_path = 'C:\\Windows\\Fonts\\arial.ttf'
+        font_path = 'C:\\Windows\\Fonts\\courbd.ttf'
     elif os.sys.platform.startswith('darwin'):
-        font_path = '/System/Library/Fonts/Supplemental/Arial.ttf'
+        font_path = '/System/Library/Fonts/Supplemental/Courier New Bold.ttf'
     elif os.sys.platform.startswith('linux'):
-        font_path = '/usr/share/fonts/truetype/msttcorefonts/arial.ttf'
+        font_path = '/usr/share/fonts/truetype/msttcorefonts/courbd.ttf'
     else:
         raise Exception("Unsupported platform")
     return font_path
 
 def add_label_to_image(image, label_text):
     img_width, img_height = image.size
-    font_size = 20
-    font_path = get_arial_font_path()
-    padding = 5
-    border_width = 5
+    font_size = 35
+    font_path = get_font_path()
+    padding = 10
+    border_thickness = 10
 
     try:
         font = ImageFont.truetype(font_path, font_size)
@@ -41,34 +42,48 @@ def add_label_to_image(image, label_text):
     _, _, _, label_height = font.getbbox(label_text)
     label_height += padding * 2
 
-    new_img = Image.new("RGBA", (img_width + border_width * 2, img_height + label_height + border_width * 2), (255, 255, 255, 0))
-    new_img.paste(image, (border_width, border_width), image)
+    new_img = Image.new("RGBA", (img_width + 2 * border_thickness, img_height + label_height + border_thickness * 2), (255, 255, 255, 0))
+    new_img.paste(image, (border_thickness, border_thickness), image)
     draw = ImageDraw.Draw(new_img)
     text_bbox = draw.textbbox((0, 0), label_text, font=font)
     text_width = text_bbox[2] - text_bbox[0]
 
     # Draw black background for the label text
-    draw.rectangle([(0, img_height + border_width), (img_width + border_width * 2, img_height + label_height + border_width * 2)], fill="black")
+    draw.rectangle([(0, img_height + border_thickness), (img_width + 2 * border_thickness, img_height + label_height + border_thickness + padding)], fill="black")
     
     # Draw white label text on the black background
-    draw.text(((img_width - text_width) / 2 + border_width, img_height + padding + border_width), label_text, font=font, fill="white")
+    draw.text(((img_width - text_width) / 2 + border_thickness, img_height + border_thickness + padding), label_text, font=font, fill="white")
+
+    # Draw black borders around the QR-code image
+    draw.rectangle([(0, 0), (img_width + 2 * border_thickness, border_thickness)], fill="black")  # top border
+    draw.rectangle([(0, 0), (border_thickness, img_height + border_thickness)], fill="black")  # left border
+    draw.rectangle([(img_width + border_thickness, 0), (img_width + 2 * border_thickness, img_height + border_thickness)], fill="black")  # right border
 
     return new_img
 
+
 def main():
-    input_file = "qr_codes_data.txt"
-    output_dir = "output"
-    os.makedirs(output_dir, exist_ok=True)
+    input_filename = "qr_codes_data.txt"
+    output_folder = "output"
+    os.makedirs(output_folder, exist_ok=True)
 
-    with open(input_file, "r") as f:
-        lines = f.readlines()
+    with open(input_filename, "r") as file:
+        for line in file:
+            qr_text = line.strip()
+            if not qr_text:
+                continue
 
-    for i, line in enumerate(lines):
-        qr_text = line.strip()
-        qr_image = create_qr_code(qr_text, output_dir)
-        qr_image_with_label = add_label_to_image(qr_image, qr_text)
-        output_path = os.path.join(output_dir, f"qr_code_{i + 1}.png")
-        qr_image_with_label.save(output_path)
+            # Check if the line starts with a letter or number followed by an underscore
+            if re.match(r"^[A-Za-z0-9]_", qr_text):
+                label_text = qr_text[2:]
+            else:
+                label_text = qr_text
+
+            qr_image = create_qr_code(qr_text, output_folder)
+            qr_image_with_label = add_label_to_image(qr_image, label_text)
+
+            output_filename = os.path.join(output_folder, f"QR_{qr_text}.png")
+            qr_image_with_label.save(output_filename)
 
 if __name__ == "__main__":
     main()
--- qrcodes.py
+++ qrcodes.py
@@ -5,8 +5,77 @@ from pyzbar import pyzbar
 import numpy as np
 from var_dump import var_dump
 import math
+from PIL import Image
+import pytesseract
 # os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = "/home/madpre/.local/lib/python3.10/site-packages/cv2/qt/plugins"
 # os.environ["QT_DEBUG_PLUGINS"] = "1"
+def preprocess_image(frame):
+    # Resize the image
+    scale_percent = 200  # percent of original size
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Apply adaptive threshold
+    threshold = cv2.adaptiveThreshold(
+        gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+    )
+
+    # Apply Gaussian blur
+    blur = cv2.GaussianBlur(threshold, (5, 5), 0)
+    # Dilate the image
+    kernel = np.ones((3, 3), np.uint8)
+    dilated = cv2.dilate(blur, kernel, iterations=1)
+    #cv2.imshow("QR Code Scanner", dilated)
+
+
+
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+    return dilated
+
+
+def detect_text(frame):
+    # Convert OpenCV image to PIL image
+    pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+    # Use Tesseract to recognize text
+    text = pytesseract.image_to_string(pil_image, lang='nor')
+    var_dump(text)
+
+    # Split the text into lines and process each line
+    lines = text.strip().split('\n')
+    names = []
+    roles = []
+
+    # Read names and roles from text files
+    with open("names.txt", "r") as f:
+        names_data = f.read().splitlines()
+
+    with open("roles.txt", "r") as f:
+        roles_data = f.read().splitlines()
+
+    for line in lines:
+        words = line.split()
+        for word in words:
+            if word in names_data:
+                names.append(word)
+            elif word in roles_data:
+                roles.append(word)
+
+    pairs = list(zip(names, roles))
+
+    # Write pairs to a text file
+    with open("paired_names_and_roles.txt", "w") as f:
+        for pair in pairs:
+            f.write(f"{pair[0]}: {pair[1]}\n")
+
+    return frame, pairs
 
 def decode_qr_codes(frame):
     # Detect QR codes in the frame
@@ -14,7 +83,7 @@ def decode_qr_codes(frame):
 
     names = []
     roles = []
-    #var_dump(decoded_objects)
+    
     
     # Read names and roles from text files
     with open("names.txt", "r") as f:
@@ -26,7 +95,13 @@ def decode_qr_codes(frame):
     # Iterate over decoded QR codes and store their positions
     for obj in decoded_objects:
         data = obj.data.decode("utf-8")
+        var_dump(data)
         position = obj.rect
+
+        x, y, w, h = obj.rect
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, data, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
         if data in names_data:
             names.append({"name": data, "position": position})
         elif data in roles_data:
@@ -67,17 +142,23 @@ def main():
     # frame = heic_to_opencv_image(heic_image_path)
     frame = cv2.imread(image_path)
 
+    # Preprocess the image
+    # preprocessed_frame = preprocess_image(frame)
+
     # Process the image and show the results
+    frame, text_pairs = detect_text(frame)
     frame, qr_codes = decode_qr_codes(frame)
-    #cv2.imshow("QR Code Scanner", frame)
+    for text_pair in text_pairs:
+        print("Text pair:", text_pair)
+    cv2.imshow("QR Code Scanner", frame)
 
     # Process the QR codes and update the overlay
     for qr_code in qr_codes:
         # Replace this with your custom logic to update the overlay
         print("QR code data:", qr_code)
 
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
